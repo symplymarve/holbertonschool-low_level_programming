02ed70e8643803c66cffa5adcf85eed258f0cbc1
symplymarve <101419536+symplymarve@users.noreply.github.com>
2022-05-13 09:52:15 +0100

    Updated task 2

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -11,22 +11,19 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
-	int (*p)(int);
-
-	p = cmp;
-
-	if (size <= 0)
-		return (-1);
-	if (!array || !cmp)
-		return (-1);
-
-	for (i = 0; i < size; i++)
-	{
-		if (p(array[i]) && array[i] != 0)
-			break;
-		if (i == size - 1)
-			return (-1);
-	}
-	return (i);
+int i;
+int (*p)(int);
+p = cmp;
+if (size <= 0)
+return (-1);
+if (!array || !cmp)
+return (-1);
+for (i = 0; i < size; i++)
+{
+if (p(array[i]) && array[i] != 0)
+break;
+if (i == size - 1)
+return (-1);
+}
+return (i);
 }
